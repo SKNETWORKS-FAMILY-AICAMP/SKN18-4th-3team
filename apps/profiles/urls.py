4990f71fd49c1ace58cff767ac462f8ffc0e5ace
Jina <jinayang1003@gmail.com>
2025-11-13 11:02:33 +0900

    feat:프로필 앱 생성

---
+++ apps/profiles/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'profiles'
+
+urlpatterns = [
+    # 마이페이지 - 프로필 조회 및 수정
+    path('', views.profile_view, name='profile'),
+    path('update/', views.profile_update_view, name='profile_update'),
+    path('password/', views.change_password_view, name='change_password'),
+    path('delete/', views.delete_account_view, name='delete_account'),
+    path('upload-image/', views.upload_profile_image_view, name='upload_profile_image'),
+
+    # 활동 로그 및 통계
+    path('activity/', views.activity_log_view, name='activity_log'),
+]
---
+++ apps/profiles/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# 프로필 이미지 수정
+
+# 유저 네임 수정
+
+# 비밀번호 수정
+
+# 대화기록 바탕의 주간, 월간 기록 표시
+    # 감정 그래프 통계
+    
